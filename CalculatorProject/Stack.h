3abe0b5cce4ddd4fc1854d790c0116d273827ae5
Pourya-Ardestani <pooryaardestani@gmail.com>
2024-07-08 02:57:52 +0330

    move Stack implementation and fixString implementation
    added implementation to the Stack.h but still can't add the method implementation to Stack.cpp
    function  fixString almost completed

--- CalculatorProject/Stack.h
+++ CalculatorProject/Stack.h
@@ -1,19 +1,60 @@
 #pragma once
-#define MAX 100
+#include<iostream>
 
 template <class T>
-class Stack
+struct Stack
 {
-private:
-	int top;
+	Stack* top;
+	Stack* next;
+	T value;
 
-public:
-	char st[MAX];
 	Stack();
-	bool push(T);
+	void push(T);
 	T pop();
 	T peek();
 	bool isEmpty();
 
 };
 
+
+
+template <class T>
+Stack<T>::Stack()
+{
+	top = NULL;
+	next = NULL;
+}
+
+
+template <class T>
+void Stack<T>::push(T new_item)
+{
+	Stack* newStack = new Stack();
+	newStack->value = new_item;
+	newStack->next = top;
+	top = newStack;
+
+}
+
+
+template <class T>
+T Stack<T>::pop()
+{
+	T x = top->value;
+	top = top->next;
+	return x;
+	////////////////khodam
+}
+
+
+template <class T>
+T Stack<T>::peek()
+{
+	return top->value;
+}
+
+template <class T>
+bool Stack<T>::isEmpty()
+{
+	return (top == NULL);
+}
